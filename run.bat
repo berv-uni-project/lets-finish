268e11e724963c56a01d37e10f22cef4e1840a75
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2016-09-26 19:52:43 +0700

    add grouping command line to run this app

---
+++ run.bat
@@ -0,0 +1,5 @@
+cd src
+gcc letsfinish.c board/board.c chance/chance.c command/command.c game/game.c mesinkata/mesinkata.c mesinkata/mesinkarakter.c player/player.c -o ../bin/letsfinish
+cd ..
+cd bin
+letsfinish
---
+++ run.sh
@@ -0,0 +1,4 @@
+cd src
+gcc letsfinish.c board/board.c chance/chance.c command/command.c game/game.c mesinkata/mesinkata.c mesinkata/mesinkarakter.c player/player.c -o ../bin/letsfinish
+cd ../bin/
+./letsfinish
